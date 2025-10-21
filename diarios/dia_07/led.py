080729c1c4ca53f03cf7cb36b1c6a4b2a9bdfa71
Brendon Gomes de Cordova <dev.gcbrendon@gmail.com>
2025-10-21 17:24:47 -0300

    docs: descriçao-desafio-LED-1168

---
+++ diarios/dia_07/led.py
@@ -0,0 +1,34 @@
+"""
+LED - 1168
+Autor Desconhecido
+Timelimit: 1
+João quer montar um painel de leds contendo diversos números. Ele não possui muitos leds, e não tem certeza se conseguirá montar o número desejado. Considerando a configuração dos leds dos números abaixo, faça um algoritmo que ajude João a descobrir a quantidade de leds necessário para montar o valor.
+
+Obs.: Para programadores de Javascript, recomenda-se o uso de "input.trim().split('\n')" para evitar erros conhecidos.
+
+
+
+
+Entrada
+A entrada contém um inteiro N, (1 ≤ N ≤ 1000) correspondente ao número de casos de teste, seguido de N linhas, cada linha contendo um número (1 ≤ V ≤ 10100) correspondente ao valor que João quer montar com os leds.
+
+Saída
+Para cada caso de teste, imprima uma linha contendo o número de leds que João precisa para montar o valor desejado, seguido da palavra "leds".
+"""
+
+
+"""
+LEDS tem a composição:
+
+1 = 2 leds
+2 = 5 leds
+3 = 5 leds
+4 = 4 leds
+5 = 5 leds
+6 = 6 leds
+7 = 3 leds
+8 = 7 leds
+9 = 6 leds
+0 = 6 leds
+
+"""
